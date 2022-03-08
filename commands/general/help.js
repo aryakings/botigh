0d512a706bab1df3c0f78878d47eef68f08ff17b
Aryabobs <93197724+aryakings@users.noreply.github.com>
2022-03-08 17:18:14 +0700

    Update and rename commands/general/help.js to perintah/umum/help.js

--- commands/general/help.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "help",
-  aliases: ["h"],
-  run: async (conn, m, args) => {
-    conn.sendDM(m.sender.pk, "Command help");
-  },
-};
---
+++ perintah/umum/help.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  run: async (conn, m, args) => {
+    conn.sendDM(m.sender.pk, "Hallo selamat datang di bot Instagram jangan lupa follow akun Instagram owner yaitu @aryahandika8 bot bisa download video tiktok dengan ketik tiktok link nya jika Instagram kirim link Instagram yg mau di download terimakasih");
+  },
+};
